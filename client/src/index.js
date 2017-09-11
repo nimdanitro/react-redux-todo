51fc25687592844d36bdd3fb2971825d29156646
Daniel Aschwanden <daa@open.ch>
2017-09-11 15:55:13 +0200

    TRIVIAL: add router

--- client/src/index.js
+++ client/src/index.js
@@ -2,26 +2,45 @@ import 'babel-polyfill'
 import React                            from 'react'
 import { render }                       from 'react-dom'
 import { Provider }                     from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
 import { composeWithDevTools }          from 'redux-devtools-extension';
 import thunk                            from 'redux-thunk';
 
+import createHistory from 'history/createBrowserHistory'
+import { Route } from 'react-router'
+
+import { ConnectedRouter, routerMiddleware } from 'react-router-redux'
+
 
 import App     from './containers/App'
-import { default as reducer } from './reducers'
+import { default as reducers } from './reducers'
 import 'todomvc-app-css/index.css'
 
 
+// Create a history of your choosing (we're using a browser history in this case)
+const history = createHistory()
+
+// Build the middleware for intercepting and dispatching navigation actions
+const router = routerMiddleware(history)
+
+
 const composeEnhancers = composeWithDevTools({});
-const store = createStore(reducer,
-  composeEnhancers(
-    applyMiddleware(thunk)
-  )
+const store = createStore(
+    reducers,
+    composeEnhancers(
+        applyMiddleware(thunk,router)
+    )
 );
 
 render(
   <Provider store={store}>
-    <App />
+    <ConnectedRouter history={history}>
+        <div>
+            <Route exact path="/" component={App}/>
+            <Route path="/about" component={App}/>
+            <Route path="/topics" component={App}/>
+        </div>
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
 )
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux'
 import { entities } from './entities'
+import { routerReducer } from 'react-router-redux'
+
 
 const rootReducer = combineReducers({
-  entities
+  entities,
+  router: routerReducer
 })
 
 export default rootReducer
